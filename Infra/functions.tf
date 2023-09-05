6b793387759742f73a811aab53ccfa4b03bae48a
Varun Verma <vermavarun@outlook.com>
2023-09-05 19:17:34 +0530

    added initial infra functions

---
+++ Infra/functions.tf
@@ -0,0 +1,20 @@
+resource "azurerm_service_plan" "vverma_app_service_plan" {
+  name                = "invvgg-app-service-plan"
+  resource_group_name = "sbox-invvgg-dev-rg"
+  location            = "West Europe"
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "vverma_linux_function_app" {
+  name                       = "vverma-linux-function-app"
+  resource_group_name        = "sbox-invvgg-dev-rg"
+  location                   = "West Europe"
+  https_only                 = true
+  storage_account_name       = "invvgg14385sa"
+  storage_account_access_key = "aZQDxxCoZfwyDTs7F9eWQn//p8nNE1VzaHO0XaDVO8mMNzxIsvTq2KdHmUK6yN8sr7hutB5U7d8x+ASt2mFLcA=="
+  service_plan_id            = azurerm_service_plan.vverma_app_service_plan.id
+
+  site_config {
+  }
+}
